b4c3d4d896c5c4bf0f028f996b1beab394110b98
Sergey Palchikov <example.example1928@gmail.com>
2022-11-13 00:29:33 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+
+if (digit % 2 == 0)
+{
+    Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("НЕТ");
+}
