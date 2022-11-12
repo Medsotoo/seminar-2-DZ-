e462eb7db60209fbdcbdd38fa4c8ce9e6f6119f3
Sergey Palchikov <example.example1928@gmail.com>
2022-11-13 00:09:39 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите первое число");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+if (digit1 > digit2)
+{
+    Console.WriteLine($"max = {digit1}");
+}
+else
+{
+    Console.WriteLine($"max = {digit2}");
+}
