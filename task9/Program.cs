f7de7f9efd385e9339c66bfbc3e8d09a002424e5
Sergey Palchikov <example.example1928@gmail.com>
2022-11-16 19:29:48 +0300

    task 9 Random

---
+++ task9/Program.cs
@@ -0,0 +1,5 @@
+﻿
+int numb = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число {numb}");
+if ((numb / 10) > (numb % 10)) {Console.WriteLine(numb / 10);}
+else {Console.WriteLine(numb % 10);}
