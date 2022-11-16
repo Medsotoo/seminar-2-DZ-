e057e2c6813e43aa08399aee08d73c18f7ac9c7c
Sergey Palchikov <example.example1928@gmail.com>
2022-11-16 21:54:39 +0300

    DZ task 15

---
+++ DZ_task15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("число");
+int digit = Convert.ToInt32(Console.ReadLine());
+if(digit == 6 || digit == 7){Console.WriteLine("ура выходной");}
+else {Console.WriteLine("не выходной");}
