38add5d7f8660b184e917d7718c7f0e0e6ccd7ac
Sergey Palchikov <example.example1928@gmail.com>
2022-11-16 21:42:19 +0300

    DZ task 13

---
+++ DZ_task13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите первое число");
+string digit = Console.ReadLine();
+if (digit.Length >= 3 & digit[0] != '-'){Console.WriteLine(digit[2]);}
+else if (digit[0] == '-' & digit.Length > 3 ){Console.WriteLine(digit[3]);}
+else{Console.WriteLine("третьего числа нет");}
+
+
+
