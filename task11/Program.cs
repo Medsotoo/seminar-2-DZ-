35d7f0f33c0129da70e8fbe33419612f422dbcc6
Sergey Palchikov <example.example1928@gmail.com>
2022-11-16 19:52:41 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,4 @@
+﻿int num1  = Convert.ToInt32(Console.ReadLine());
+int numb2 = Convert.ToInt32(Console.ReadLine());
+if(num1 % numb2 == 0) {Console.WriteLine($"кратно");}
+else {Console.WriteLine($"не кратно, остаток {num1 % numb2}");}
