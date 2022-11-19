c7d210a6505bd6f680813cb22d847707f46f1473
Sergey Palchikov <example.example1928@gmail.com>
2022-11-19 14:43:56 +0300

    DZ_task 21

---
+++ DZ_task21/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите координаты первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine()), y1 = Convert.ToInt32(Console.ReadLine()),z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координаты второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine()), y2 = Convert.ToInt32(Console.ReadLine()),z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("расстояние между ними ");
+Console.WriteLine(Math.Sqrt((Math.Pow(x2 - x1,2)) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2)));
