d669f9582aecea0d59df57f34e5c62537232883f
Sergey Palchikov <example.example1928@gmail.com>
2022-11-19 14:29:25 +0300

    task 19 Palindrom

---
+++ DZ_task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите пятизначное число");
+string numb = Console.ReadLine();
+int sum = 0;
+
+for (int i = 1; i < 3; i++)
+{
+    if (numb[i - 1] == numb[^i])
+    {
+        sum += 1;
+    }
+
+}
+
+if (numb.Length < 5 || numb.Length > 5) { Console.WriteLine("число не пятизначное"); }
+else if (sum == 2){Console.WriteLine("число палиндром");}
+else{Console.WriteLine("число не палиндром");}
