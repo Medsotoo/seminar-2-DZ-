20e72bd1fba81197f6a944e02a45b14c6567ca43
Sergey Palchikov <example.example1928@gmail.com>
2022-11-23 11:14:08 +0300

    DZ_ task 29 result

---
+++ DZ_task29/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите длинну массива");
+int count = Convert.ToInt32(Console.ReadLine());
+
+
+int [] array = new int [count];
+
+for (int i = 0; i < count; i++)
+{
+    array[i] = new Random().Next(0,20);
+    Console.Write(" " + array[i]);
+}
