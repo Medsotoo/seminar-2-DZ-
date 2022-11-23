1615966ebd29349f4c0dda32cb2bcc0057ab2bd2
Sergey Palchikov <example.example1928@gmail.com>
2022-11-23 10:59:17 +0300

    DZ_ task 25

---
+++ DZ_task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите степень числа");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = a;
+for(int i = 2; i <= b; i++ )
+{
+    res *= a;
+}
+Console.WriteLine(res);
