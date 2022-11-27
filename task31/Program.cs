6b21fee4fdf2867a0b6fa1797cab38e0bff71108
Sergey Palchikov <example.example1928@gmail.com>
2022-11-27 19:35:53 +0300

    task 31 seminar 5

---
+++ task31/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите длинну массива");
+int count = Convert.ToInt32(Console.ReadLine());
+
+
+int [] array = new int [count];
+
+for (int i = 0; i < count; i++)
+{
+    array[i] = new Random().Next(-9,10);
+    Console.Write(" " + array[i]);
+}
+
+Console.WriteLine();
+
+for(int i = 0; i < count; i++)
+{
+    if (array[i] < 0){array[i] = Math.Abs(array[i]);}
+    else{array[i] = -(array[i]); }
+    Console.Write(" " + array[i]);
+}
