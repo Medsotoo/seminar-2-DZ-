bf6e1f9a4b771669b5c1a530d77a9d7462fa6a1e
Sergey Palchikov <example.example1928@gmail.com>
2022-11-27 20:34:58 +0300

    DZ task 36

---
+++ DZ_task36/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] list(int leni)
+{
+    int [] lst = new int [leni];
+    for(int i = 0; i < leni; i++)
+    {
+        lst[i] = new Random().Next(0,100);
+    }
+    return lst;
+}
+
+Console.WriteLine("введите длину массива ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = list(len);
+Console.WriteLine(string.Join(" ",array));
+
+
+int result = 0;
+for(int i = 1; i < array.Length; i += 2)
+{
+    result += array[i];
+}
+Console.WriteLine(result);
+
+
+
+
