ed200454575cdc88b5b962f2c4aa84a413f95739
Sergey Palchikov <example.example1928@gmail.com>
2022-12-01 16:23:13 +0300

    DZ_task 41

---
+++ DZ_task41/Program.cs
@@ -0,0 +1,7 @@
+﻿String[] array = Console.ReadLine().Split(','); 
+int count = 0;
+for(int i = 0;i < array.Length; i++)
+{
+    if(Convert.ToInt32(array[i]) > 0){count += 1;}
+}
+Console.WriteLine(count);
