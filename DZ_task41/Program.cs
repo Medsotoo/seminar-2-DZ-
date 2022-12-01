b50eb23475e9395f7338b73a6e2b1e9c78474e62
Sergey Palchikov <example.example1928@gmail.com>
2022-12-01 17:07:39 +0300

    DZ_task 41

--- DZ_task41/Program.cs
+++ DZ_task41/Program.cs
@@ -1,7 +1,8 @@
-﻿String[] array = Console.ReadLine().Split(','); 
+﻿Console.WriteLine("Ведите список чисел");
+String[] array = Console.ReadLine().Split(','); 
 int count = 0;
 for(int i = 0;i < array.Length; i++)
 {
     if(Convert.ToInt32(array[i]) > 0){count += 1;}
 }
-Console.WriteLine(count);
+Console.WriteLine($"количество положительных чисел {count}");
