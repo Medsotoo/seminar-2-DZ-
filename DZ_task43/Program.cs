952a9ef1ea489c4834fa7770c1344d2f219733bb
Sergey Palchikov <example.example1928@gmail.com>
2022-12-01 21:15:04 +0300

    DZ_TASk 43

---
+++ DZ_task43/Program.cs
@@ -0,0 +1,15 @@
+﻿double k1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double x = 0;
+double y = 0;
+
+if(k1 == k2 & b1 == b2){Console.WriteLine("Прямые совпадают");}
+else if(k1 == k2 & b1 != b2){Console.WriteLine("Прямые параллельны");}
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+}
+Console.WriteLine($"({x};{y})");
