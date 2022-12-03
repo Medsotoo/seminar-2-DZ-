b6e6c6082315216b17305981eba853a5dd1698ef
Sergey Palchikov <example.example1928@gmail.com>
2022-12-03 11:14:38 +0300

    DZ_task 50

---
+++ DZ_task50/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("введите количество сторк в массиве :");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов в массиве :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("укажите позицию элемента :");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+bool flag = false;
+int result = 0;
+
+int [,] matrix = new int[m,n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(-10,10);
+         Console.Write(matrix[i,j] + " ");
+        if(i == x & j == y){flag = true;result = matrix[i,j];}
+    } 
+    Console.WriteLine();
+}
+if(flag){Console.Write(result);}
+else{Console.Write(" Такого элемента нет в матрице ");}
