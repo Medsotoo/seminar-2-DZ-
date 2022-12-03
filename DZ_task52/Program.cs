9c5bbb7f9ca8d8ec86a46dd5e41fdc107f86f8aa
Sergey Palchikov <example.example1928@gmail.com>
2022-12-03 11:33:42 +0300

    DZ_task 52

---
+++ DZ_task52/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("введите количество сторк в массиве :");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов в массиве :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double[m,n];
+for(int i = 0; i < m; i++)
+{   
+    double res = 0;
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i,j] = Math.Round(new Random().Next(-10,10) + new Random().NextDouble(),2);
+        Console.Write(matrix[i,j] + " ");
+    }    
+    Console.WriteLine();
+}
+
+for(int i = 0; i < m; i++)
+{   
+    double res = 0;
+    for(int j = 0; j < n; j++)
+    {
+        res += matrix[j,i];
+    }    
+    Console.WriteLine($"среднее арифметическое первого столбца : {Math.Round(res / m,2)}");
+}
