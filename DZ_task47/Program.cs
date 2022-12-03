73f4d48c292f92865cb0f3ce9bcd89b547e987fe
Sergey Palchikov <example.example1928@gmail.com>
2022-12-03 10:54:17 +0300

    DZ_task 47

---
+++ DZ_task47/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("введите количество сторк в массиве :");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите количество столбцов в массиве :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double[m,n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(-10,10) + Math.Round(new Random().NextDouble(),2);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
