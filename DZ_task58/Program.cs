df762d0b948ca431008aefbd51f3c065ad4d6d7c
Sergey Palchikov <example.example1928@gmail.com>
2022-12-12 22:23:21 +0300

    DZ_task58

---
+++ DZ_task58/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int[,] NewArray(int m,int n)
+{
+    int [,] matrix = new int[m,n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void Printmatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+Console.WriteLine("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число строк 1-й матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов 2-й матрицы: ");
+int p = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix1 = NewArray(m,n);
+int[,] matrix2 = NewArray(n,p); 
+
+Printmatrix(matrix1);
+Console.WriteLine();
+Printmatrix(matrix2);
+Console.WriteLine();
+
+int[,] result = new int[n,p];
+
+MultiplyMatrix(matrix1,matrix2,result);
+Printmatrix(result);
+
+
+
+
+
+
+
+
+
